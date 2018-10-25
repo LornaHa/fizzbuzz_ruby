86c21898d55213119d6b8217a1db65532b16db2b
Lorna Harwood <lorna@Lornas-MacBook-Pro.local>
2018-10-25 14:15:56 +0100

    rspec test for fizzbuzz and 1

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,17 @@
 def fizzbuzz(number)
 
+for num in 1..20
+
   if number == 3
     return "fizz"
   elsif number == 5
     return "buzz"
+  elsif number == 15
+    return "fizzbuzz"
+  else
+    return number
   end
 
+
+end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -11,3 +11,15 @@ describe "fizzbuzz" do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 end
+
+describe "fizzbuzz" do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
+
+describe "fizzbuzz" do
+  it 'returns "1" when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+end
