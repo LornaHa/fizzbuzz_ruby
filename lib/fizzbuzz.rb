4655db73b61dfdba771e71318afe6c4985ff1295
Lorna Harwood <lorna@Lornas-MacBook-Pro.local>
2018-10-25 13:50:19 +0100

    created a fizzbuzz file and spec file which tests fizz and buzz

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(number)
+
+  if number == 3
+    return "fizz"
+  elsif number == 5
+    return "buzz"
+  end
+
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,13 @@
+require 'fizzbuzz'
+
+describe "fizzbuzz" do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
+
+describe "fizzbuzz" do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
